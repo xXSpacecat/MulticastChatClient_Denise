9ef68ef65f85e1940d2dd749d7c8ee7bfd404fbb
denise.leon <denise.leon@elev.ga.ntig.se>
2024-05-13 09:25:25 +0200

    last commit

--- src/MulticastClient.java
+++ src/MulticastClient.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.net.*;
 
 public class MulticastClient {
-    public static void main(String[] args) {
-        throws Exception{
+    public static void main(String[] args) throws Exception{
 
             //default portnumber
             int portNumber = 1235;
@@ -17,7 +16,21 @@ public class MulticastClient {
             InetAddress group = InetAddress.getByName("225.4.5.6");
 
             //Join multicast group
+            chatMulticastSocket.joinGroup(group);
+
+            //User enters message
+            String msg = " ";
+            System.out.println("Type message to server: ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            msg = br.readLine();
+
+            //send message to Multicast
+             DatagramPacket data = new DatagramPacket(msg.getBytes(), 0, msg.length(), group, portNumber);
+             chatMulticastSocket.send(data);
+
+             //close socket
+            chatMulticastSocket.close();
         }
     }
 
-}
+
